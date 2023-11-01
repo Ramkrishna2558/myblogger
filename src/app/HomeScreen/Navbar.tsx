e6bde10199696ab0817c67360d86b3a4028a3c53
ram <ramkrishnamore299@gmail.com>
2023-11-01 14:45:47 +0530

    myblogxy 1st commit

---
+++ src/app/HomeScreen/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+export default function navbar(){
+    return (
+<nav>
+    <div>
+        <h2 className="">logo here</h2>
+    </div>
+</nav>
+    );
+}
---
+++ src/app/HomeScreen/homescreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HomeScreen(){
+    return (
+        <>
+        <h2>no body here</h2>
+        </>
+    )
+}
